ada62444e390e1641a20ce3cbbb753f449b071d7
Leonardo Martin <leowork2002@gmail.com>
2024-03-15 17:23:47 -0300

    Sketch of ppos_core.c almost completed

--- SegundoProjeto/P2/ppos_core.c
+++ SegundoProjeto/P2/ppos_core.c
@@ -1,20 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "ppos.h"
+
+#define STACKSIZE 64*1024	/* tamanho de pilha das threads */
+
+task_t main_task;
+task_t current_task;
 
 // Inicializa o sistema operacional; deve ser chamada no inicio do main()
-void ppos_init () ;
+void ppos_init ()
+{
+    main_task.id = 0;
+    main_task.prev = NULL;
+    main_task.next = NULL;
+    main_task.status = 1;
+
+    // current_task = main_task; id?
+}
 
 // gerência de tarefas =========================================================
 
 // Inicializa uma nova tarefa. Retorna um ID> 0 ou erro.
-int task_init (task_t *task,			// descritor da nova tarefa
-               void  (*start_func)(void *),	// funcao corpo da tarefa
-               void   *arg) ;			// argumentos para a tarefa
+// (descritor da nova tarefa, funcao corpo da tarefa, argumentos para a tarefa)
+int task_init (task_t *task, void  (*start_func)(void *), void   *arg)
+{
+    char *stack;
+
+    getcontext (&(task->context));
+   // Captura o contexto atual e o armazena em ContextPing
+
+   stack = malloc (STACKSIZE) ;
+   if (stack)
+   {
+      task->context.uc_stack.ss_sp = stack;
+      task->context.uc_stack.ss_size = STACKSIZE ;
+      task->context.uc_stack.ss_flags = 0 ;
+      task->context.uc_link = 0 ;
+   }
+   else
+   {
+      perror ("Erro na criação da pilha: ") ;
+      exit (1) ;
+   }
+
+   makecontext (&(task->context), start_func, 1, (char *)arg) ;
+}			
+
+// alterna a execução para a tarefa indicada
+int task_switch (task_t *task) 
+{
+    if(swapcontext (&current_task, task))
+    {
+        current_task = *task;
+        return 0;
+    }
+    
+    //else
+    return -1;
+}
 
 // retorna o identificador da tarefa corrente (main deve ser 0)
-int task_id () ;
+int task_id() 
+{
+    return current_task.id;
+}
 
 // Termina a tarefa corrente com um status de encerramento
 void task_exit (int exit_code) ;
 
-// alterna a execução para a tarefa indicada
-int task_switch (task_t *task) ;
